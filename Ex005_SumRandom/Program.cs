f63c6734b7f819305ebfdce03a9a374caf81df87
Mark M <medvedevmark1999@gmail.com>
2022-10-28 18:25:39 +0300

    Задача на нахождение двух рандомных чисел

---
+++ Ex005_SumRandom/Program.cs
@@ -0,0 +1,6 @@
+﻿int numA = new Random().Next(1, 10);  // 1 2 3 ... 9
+Console.WriteLine(numA);
+int numB = new Random().Next(1, 20); // 1 2 3 ... 19
+Console.WriteLine(numB);
+int result = numA + numB;
+Console.WriteLine(result);
