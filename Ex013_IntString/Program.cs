37d4feaeb51a16904074ce94b2a7c70008331f19
Mark M <medvedevmark1999@gmail.com>
2022-11-01 22:43:54 +0300

    IntStraight

---
+++ Ex013_IntString/Program.cs
@@ -0,0 +1,23 @@
+﻿// Показывает год
+
+//int Method3()
+//{
+//    return DateTime.Now.Year;
+//}
+//int year = Method3();
+//Console.WriteLine(year);
+
+
+string Method4(string msg, int count)
+{
+    int i = 0;
+    string result = String.Empty;
+    while(i< count)
+    {
+        result = result + msg;
+        i++;
+}
+return result;
+}
+string text = Method4("qw", 3);
+Console.WriteLine(text);
