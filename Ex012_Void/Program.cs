e44da13d49695b3821efe3210e575ef9efcd9aaf
Mark M <medvedevmark1999@gmail.com>
2022-11-01 22:17:43 +0300

    Void

---
+++ Ex012_Void/Program.cs
@@ -0,0 +1,20 @@
+﻿//Console.Clear();
+//void Method1 (string msg)
+//{
+//    Console.WriteLine(msg);
+//}
+//Method1 ("Привет!");
+
+
+
+Console.Clear();
+void Method2(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}  
+    Method2("Good day!", 4);
