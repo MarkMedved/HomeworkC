bde376992d93f377447af321ccca674f314caa7d
Mark M <medvedevmark1999@gmail.com>
2022-11-12 19:42:28 +0300

    Recursia

---
+++ Ex_recursia/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+int m = 2;
+int n = 10;
+int k = 1;
+int q = 2;
+
+int res = GetWays(m, n, k, q);
+
+int GetWays(int m, int n, int k, int q, string text = " ")
+{
+    Console.WriteLine($"{m,3}{n,3}{k,3}{q,3}{text}");
+    if (n < m) return 0;
+    if (n == m) return 1;
+    else if (n % q != 0) return GetWays(m, n - k, k, q, text + $"({n}-{k})");
+    else return GetWays(m, n - k, k, q, text + $"({n} –– {k})") + GetWays(m, n / q, k, q, text + $"({n}➗{q})");
+}
+Console.WriteLine(res);
